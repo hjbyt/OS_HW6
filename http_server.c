ac18535cac71248567e759981376ae11bf1e8c02
hjbyt <sopu00@gmail.com>
2016-05-23 14:54:20 +0300

    ...

--- http_server.c
+++ http_server.c
@@ -24,6 +24,10 @@
 // Macros
 //
 
+//TODO: uncomment
+#define ENABLE_DEBUG_PRINTS
+
+
 // Return number of elements in static array
 #define ARRAY_LENGTH(array) (sizeof(array)/sizeof(array[0]))
 // Exit with an error message
@@ -38,6 +42,12 @@
 			exit(EXIT_FAILURE);                  \
 		}                                        \
 	} while (0)
+
+#ifdef ENABLE_DEBUG_PRINTS
+#define PRINTF printf
+#else
+#define PRINTF
+#endif
 
 //
 // Constants
@@ -54,6 +64,8 @@ typedef int bool;
 
 #define REQUEST_MAX_SIZE 8192
 
+#define HTTP_OK "HTTP/1.0 200 OK\n\n"
+
 //
 // Structs
 //
@@ -68,6 +80,9 @@ typedef int bool;
 
 void handle_request(int client_fd);
 char* parse_request(char* request, int length, bool* is_post);
+void handle_post_request(int client_fd, const char* path);
+void handle_get_request(int client_fd, const char* path);
+void write_all(int fd, void* data, int size);
 
 //
 // Implementation
@@ -103,9 +118,11 @@ int main(int argc, char** argv)
 	VERIFY(bind(listen_fd, (struct sockaddr*)&server_address, sizeof(server_address)) != -1, "bind failed");
 	VERIFY(listen(listen_fd, workers_count) != -1, "listen failed");
 
+	PRINTF("Server started\n");
 	while (TRUE)
 	{
 		//TODO: use select() ?
+		PRINTF("wait for connection\n");
 		int client_fd = accept(listen_fd, NULL, NULL);
 		VERIFY(client_fd != -1, "accept failed");
 
@@ -119,6 +136,7 @@ int main(int argc, char** argv)
 
 void handle_request(int client_fd)
 {
+	PRINTF("got request\n");
 	char* buffer = (char*)malloc(REQUEST_MAX_SIZE);
 	VERIFY(buffer != NULL, "malloc failed");
 
@@ -126,12 +144,17 @@ void handle_request(int client_fd)
 	int bytes_read = read(client_fd, buffer, REQUEST_MAX_SIZE);
 	VERIFY(bytes_read != -1, "read from client socket failed");
 	CHECK(bytes_read != 0, "client disconnected unexpectedly");
+	PRINTF("request: %s\n", buffer);
 
 	bool is_post;
 	char* path = parse_request(buffer, bytes_read, &is_post);
 	CHECK(path != NULL, "invalid request");
 
-
+	if (is_post) {
+		handle_post_request(client_fd, path);
+	} else {
+		handle_get_request(client_fd, path);
+	}
 
 	free(buffer);
 }
@@ -151,4 +174,59 @@ char* parse_request(char* request, int length, bool* is_post)
 	}
 
 	return strtok(s, " ");
+}
+
+void handle_post_request(int client_fd, const char* path)
+{
+
+}
+
+void handle_get_request(int client_fd, const char* path)
+{
+	PRINTF("Get (%s)\n", path);
+	struct stat path_stat;
+	if (stat(path, &path_stat) == -1) {
+		if (errno == ENOENT) {
+			// TODO: send 404 not found
+		} else {
+			// TODO: return 500 internal error
+		}
+		return;
+	}
+
+	// TODO: check if is file / dir
+
+	int fd = open(path, O_RDONLY);
+	if (fd == -1) {
+		// TODO: send 500 internal error
+		return;
+	}
+
+	char* buffer = (char*)malloc(MEGA);
+	VERIFY(buffer != NULL, "malloc failed");
+	int bytes_read = 0;
+	PRINTF("Send file\n");
+	write_all(client_fd, HTTP_OK, ARRAY_LENGTH(HTTP_OK) - 1);
+	while (TRUE)
+	{
+		bytes_read = read(fd, buffer, MEGA);
+		VERIFY(bytes_read != -1, "read requested file failed");
+		if (bytes_read == 0) {
+			break;
+		}
+		write_all(client_fd, buffer, bytes_read);
+	}
+
+	close(fd);
+}
+
+void write_all(int fd, void* data, int size)
+{
+	assert(size >= 0);
+	while (size > 0) {
+		int bytes_written = write(fd, data, size);
+		VERIFY(bytes_written != -1, "write response failed");
+		size -= bytes_written;
+		assert(size >= 0);
+	}
 }
